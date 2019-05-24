0c8110265120d88a9a516dfb5e3ef2937982a59b
jeremyNCH <jnchafok@uwaterloo.ca>
2019-05-23 22:34:23 -0400

    updating test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -14,3 +14,35 @@ beforeEach(async () => {
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 });
+
+describe('Lottery contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+
+  it('allows 1 account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[0]);
+    assert.equal(accounts[2], players[0]);
+    assert.equal(1, players.length);
+  });
+});
