f6cbc2b83e65ecf98ab2f59649b9fce0b7572cd2
jeremyNCH <jnchafok@uwaterloo.ca>
2019-05-23 23:02:20 -0400

    update tests and fix node package

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -45,4 +45,42 @@ describe('Lottery contract', () => {
     assert.equal(accounts[2], players[0]);
     assert.equal(1, players.length);
   });
+
+  it('requires min amt of ether', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 200
+      });
+      assert(false);
+    } catch(err) {
+      assert(err);
+    }
+  });
+
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1],
+        value: web3.utils.toWei('0.02', 'ether')
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('sends money to winner and reset players', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    });
+    
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const diff = finalBalance - initialBalance;
+    
+    assert(diff > web3.utils.toWei('1.8', 'ether'));
+  });
 });
